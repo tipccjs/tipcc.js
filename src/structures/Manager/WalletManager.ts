d06723e0baa15bdfdc4ae1a487d136c1841effe4
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2023-04-25 22:02:40 +0200

    feat(WalletManager): Add totalUSD

--- src/structures/Manager/WalletManager.ts
+++ src/structures/Manager/WalletManager.ts
@@ -1,6 +1,8 @@
+import BigNumber from 'bignumber.js';
 import { Wallet } from '../Wallet';
 import { TipccClient } from '../TipccClient';
 import { Cache } from '../Cache';
+import { FiatCurrency } from '../Currency';
 import {
   RESTGetAPIAccountWalletResult,
   RESTGetAPIAccountWalletsResult,
@@ -24,12 +26,16 @@ export class WalletManager {
   ): Promise<Wallet | null> {
     if (cache && this.cache.has(currencyCode))
       return this.cache.get(currencyCode)!;
+
     const wallet = (await this.client.REST.get(
       Routes.accountWallet(currencyCode),
     )) as RESTGetAPIAccountWalletResult;
+
     if (!wallet) return null;
+
     const w = new Wallet(wallet, this.client);
     if (w.currency) this.cache.set(w.currency.code, w);
+
     return w;
   }
 
@@ -47,9 +53,26 @@ export class WalletManager {
     const { wallets } = (await this.client.REST.get(
       Routes.accountWallets(),
     )) as RESTGetAPIAccountWalletsResult;
+
     if (!wallets) return [];
+
     const ws = wallets.map((wallet) => new Wallet(wallet, this.client));
     ws.forEach((w) => w.currency && this.cache.set(w.currency.code, w));
+
     return ws;
+  }
+
+  public async totalUSD(forceNewValues = false): Promise<BigNumber> {
+    if (forceNewValues) await this.fetchAll();
+
+    let total = BigNumber(0);
+    for (const wallet of this.cache.values()) {
+      if (!wallet.usdValue) continue;
+      total = total.plus(wallet.usdValue?.valueRaw);
+    }
+
+    if (!this.client.fiats.get('USD')) this.client.fiats.refresh();
+
+    return (this.client.fiats.get('USD') as FiatCurrency).convertFromRaw(total);
   }
 }
